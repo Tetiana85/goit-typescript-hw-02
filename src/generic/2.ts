073665d014d12d939db869dc54879941d7531302
TetianaL <4215654@gmail.com>
2024-02-07 18:20:55 +0200

    1.1.1

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -8,16 +8,18 @@ type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
-
-function compare (top, bottom): AllType {
+  weight: number;
+};
+function compare(
+  top: Pick<AllType, "name" | "color">,
+  bottom: Pick<AllType, "position" | "weight">
+): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
